aebde99e598949a560736b754265604364e0a896
Ayomiposi22333 <155584008+Ayomiposi22333@users.noreply.github.com>
2024-01-10 00:21:30 +0100

    Update config.js

--- config.js
+++ config.js
@@ -7,14 +7,14 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['233533763772', 'Abraham Dwamena', true],
-  ['923184070915', '𝙎𝘼𝙇𝙈𝘼𝙉 𝘼𝙃𝙈𝘼𝘿 -(𝘼𝙝𝙢𝙢𝙞)', true], 
+  ['2349132681483', 'Abraham Dwamena', true],
+  ['', '𝙎𝘼𝙇𝙈𝘼𝙉 𝘼𝙃𝙈𝘼𝘿 -(𝘼𝙝𝙢𝙢𝙞)', true], 
   ['918360234087', 'WHITE', true], 
 ] //Number of owners
 
 //global.pairingNumber = "" //put your bot number here
 
-global.mods = ['233533763772','923184070915','923184474176'] 
+global.mods = ['2349132681483','923184070915','923184474176'] 
 global.prems = ['923184070915', '233533763772', '918360234087']
 global.allowed = ['923184070915', '233533763772', '918360234087']
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
@@ -53,8 +53,8 @@ global.APIKeys = { // APIKey Here
 // Sticker WM
 global.botname = 'XLICON-V2'
 global.premium = 'true'
-global.packname = 'XLICON TEAM' 
-global.author = '@XLICON-V2' 
+global.packname = 'king'
+global.author = 'king' 
 global.menuvid = 'https://telegra.ph/file/325630f66abc968eda8e2.mp4'
 global.igfg = '▢✓ Follow My channel\nhttps://whatsapp.com/channel/0029VaE8GbCDzgTILE7OtC3e\n' 
 global.dygp = 'https://chat.whatsapp.com/BfH0KLkICn2BjmGFMRcGMW'
